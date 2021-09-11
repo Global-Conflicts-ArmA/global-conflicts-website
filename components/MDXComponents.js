f2b9973bcfa0ac380e0057f8aa39c746bc4f5c53
VyMajoris <vycanismajoriscsa@gmail.com>
2021-09-10 23:35:58 -0300

    wip

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/display-name */
 import { useMemo } from "react";
 import { getMDXComponent } from "mdx-bundler/client";
-import Image from "./Image";
+import Image from "./mdx_components/Image";
 import CustomLink from "./mdx_components/Link";
 import code from "./mdx_components/code";
 
