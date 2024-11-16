1b411eea3f2f8e522c653c0d413196c54c4f76d6
Joe White <arceo94.jw@gmail.com>
2024-11-16 08:32:40 +0000

    use className instead of class on new svgs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -141,7 +141,7 @@ export default function MyApp({
 										width="24" 
 										height="24" 
 										fill="currentColor" 
-										class="bi bi-tiktok" 
+										className="bi bi-tiktok" 
 										viewBox="0 0 16 16"
 									>
 										<path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"/>
@@ -159,7 +159,7 @@ export default function MyApp({
 										width="24" 
 										height="24" 
 										fill="currentColor" 
-										class="bi bi-twitter-x" 
+										className="bi bi-twitter-x" 
 										viewBox="0 0 16 16"
 									>
 										<path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
